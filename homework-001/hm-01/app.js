cdf59e5de71f740b867f7b3a5db395ead9d96bdf
Tetiana <jany.starling@gmail.com>
2025-07-24 15:53:34 +0300

    Homework 3.1

---
+++ homework-001/hm-01/app.js
@@ -0,0 +1,11 @@
+console.log('Type of undefined is', typeof undefined);
+console.log('Type of 0 is', typeof 0);
+console.log('Type of true is', typeof true);
+console.log('Type of false is', typeof false);
+console.log('Type of "hello" is', typeof "hello");
+console.log('Type of {} is', typeof {});
+console.log('Type of null is', typeof null);
+console.log('Type of function(){} is', typeof function(){});
+console.log('Type of NaN is', typeof NaN);
+console.log('Type of Infinity is', typeof Infinity);
+console.log('Type of [] is', typeof []);
