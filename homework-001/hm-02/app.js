8e95dbcbaed1ee2ab1c721184117a7b1c642c20e
Tetiana <jany.starling@gmail.com>
2025-07-24 18:23:31 +0300

    Removed comments

--- homework-001/hm-02/app.js
+++ homework-001/hm-02/app.js
@@ -1,4 +1,2 @@
-// Отримати від користувача 3 рядки та вивести їх у довільному порядку однією командою (шаблонні рядки);
-
 const userStrings = `${prompt('Enter something')} ${prompt('Enter something')} ${prompt('Enter something')}`;
 alert(`User entered ${userStrings}`);
