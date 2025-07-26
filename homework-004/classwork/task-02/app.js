0b002127f2a36a8329dbf04c3b1a1d22c3c2fc86
Tetiana <jany.starling@gmail.com>
2025-07-26 20:32:22 +0300

    Homework Lecture 4

--- homework-004/classwork/task-02/app.js
+++ homework-004/classwork/task-02/app.js
@@ -1,12 +1,18 @@
 // Task #2-----------------------------------
 // Відомі 2 відстані. Одна - в кілометрах, інша - в футах (1 фут = 0,305м, 1км = 1000м). Яка відстань менша?
-const dis1 = parseInt(prompt('Enter distance feet')) * 0.035;
-const dis2 = parseInt(prompt('Enter distance kilometers'));
+const dis1 = parseInt(prompt('Enter distance feet'), 10) * 0.305;
+const dis2 = parseInt(prompt('Enter distance kilometers'), 10) * 1000;
 
 if (dis1 > dis2) {
-    console.log('Distance #1 in kilometers =', dis1, 'distance #1 > than distance #2');
+    console.log(`Distance #1 in meters = ${dis1}, 
+    Distance #2 in meters = ${dis2},
+    distance #1 > than distance #2`);
 } else if (dis1 < dis2) {
-    console.log('Distance #1 in kilometers =', dis1, 'distance #1 < than distance #2');
+    console.log(`Distance #1 in meters = ${dis1}, 
+    Distance #2 in meters = ${dis2},
+    distance #1 < than distance #2`);
 } else {
-    console.log('distance #1 = distance #2');
+    console.log(`Distance #1 in meters = ${dis1}, 
+    Distance #2 in meters = ${dis2},
+    distance #1 = than distance #2`);
 }
--- homework-004/classwork/task-03/app.js
+++ homework-004/classwork/task-03/app.js
@@ -4,8 +4,8 @@ const a = parseInt(prompt('Enter number 1'));
 const b = parseInt(prompt('Enter number 2'));
 
 if (a < b) {
-    console.log(b % a === 0 ? 'YES' : 'NO');
+    console.log(b % a === 0 ? 'Number a is a divisor of number b' : 'Number a is NOT a divisor of number b');
 } else {
     console.log('a > b')
-    console.log(a % b === 0 ? 'YES' : 'NO');
+    console.log(a % b === 0 ? 'Number b is a divisor of number a' : 'Number b is NOT a divisor of number a');
 }
