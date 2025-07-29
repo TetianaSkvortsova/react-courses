a422ec716857649e74a51e916723dc37b06b0990
Tetiana <jany.starling@gmail.com>
2025-07-29 15:10:39 +0300

    Homework Lecture 5 Bag Fixing hw-5_1

--- homework-005/hw-5_1/app.js
+++ homework-005/hw-5_1/app.js
@@ -1,6 +1,13 @@
 // Task #5-1--------------------------------
 // Вивести в консоль числа від 20 до 30 через пропуск використовуючи крок 0,5 (20 20,5 21 21,5 ....)
-document.write(`Numbers from 20 to 30 step 0.5 <br>`);
+// document.write(`Numbers from 20 to 30 step 0.5 <br>`);
+let stringNum = '';
 for (let i = 20; i <= 30; i += 0.5) {
-    document.write(`<span>${i} </span>`);
+    if (i === 30) {
+        stringNum += i;
+    } else {
+        stringNum += i;
+        stringNum += ', ';
+    }
 }
+console.log(stringNum);
