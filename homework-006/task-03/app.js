9bff036b784b00aaf5f488c2340a299efbb316dc
Tetiana <jany.starling@gmail.com>
2025-08-01 17:19:49 +0300

    Homework Lecture 6 task 3 bag fix

--- homework-006/task-03/app.js
+++ homework-006/task-03/app.js
@@ -1,11 +1,13 @@
 // Task #3--------------------------------
 // Дано масив об’єктів. Вивести масив телефонних номерів користувачів в яких баланс більше ніж 2000 доларів. І знайти суму всіх балансів користувачів
 let allBalances = 0;
+const phoneArr = [];
 for (let i = 0, length = users.length; i < length; i++) {
     const balance = users[i].balance;
     allBalances += balance;
     if (balance > 2000) {
-        document.write(`<p>Phone number: ${users[i].phone}</p>`)
+        phoneArr.push(users[i].phone);
     }
 }
-document.write(`<p>All balances: ${allBalances.toFixed(2)}</p>`)
+document.write(`<p>Phone numbers where balances > 2000:<br>${phoneArr.join('<br>')}</p>`)
+document.write(`<p>Sum of all balances: ${allBalances.toFixed(2)}</p>`)
