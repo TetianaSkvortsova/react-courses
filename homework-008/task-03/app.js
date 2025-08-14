3e0aea9a254e9c3756aff4601163667e9ce7ee8d
Tetiana <jany.starling@gmail.com>
2025-08-14 19:30:27 +0300

    Homework Lecture 8 HW fixed 7.3

--- homework-008/task-03/app.js
+++ homework-008/task-03/app.js
@@ -2,7 +2,7 @@
 
 function showLastNumber() {
     for (let i = 0; i < 10; i++) {
-        const number = parseInt(prompt('Enter a number > 100'), 10);
+        const number = prompt('Enter a number > 100');
         if ((number > 100 && number) || (i === 9)) {
             console.log(`The last number is ${number}`);
             break;
