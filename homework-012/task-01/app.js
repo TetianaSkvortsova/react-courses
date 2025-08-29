c15800f6b28bf75b93256876e626d60980c9fad2
Tetiana <jany.starling@gmail.com>
2025-08-29 19:53:48 +0300

    Homework Lecture 12 done

--- homework-012/task-01/app.js
+++ homework-012/task-01/app.js
@@ -1,6 +1,26 @@
 // Task #1--------------------------------
+// Вивести таблицю Піфагора (10×10), таблиця повинна бути створена динамічно
 
 'use strict'
 
+const tableP = document.querySelector('.wrapper');
+
+for (let i = 0; i < 11; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+    tableP.appendChild(row);
+    for (let j = 0; j < 11; j++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        row.appendChild(cell);
+        if (i === 0 && j > 0) {
+            cell.innerHTML = `${j}`;
+        } else if (j === 0 && i > 0) {
+            cell.innerHTML = `${i}`;
+        } else {
+            cell.innerHTML = `${i * j}`;
+        }
+    }
+}
 
 
--- homework-012/task-03/app.js
+++ homework-012/task-03/app.js
@@ -4,3 +4,34 @@
 
 'use strict'
 
+const prImages = document.querySelector('.pr-images');
+const imagesNum = document.querySelector('.images-num');
+const randomImage = document.querySelector('.random-image');
+
+for (let i = 0; i < 9; i++) {
+    const prImage = document.createElement('div');
+    prImage.classList.add('pr-image');
+    prImages.appendChild(prImage);
+    prImage.style.backgroundImage = `url('images/${i + 1}.png')`;
+
+    const imageNum = document.createElement('div');
+    imageNum.classList.add('image-num');
+    imagesNum.appendChild(imageNum);
+    imageNum.innerHTML = `${i + 1}`;
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const randomNumber = getRandomInt(1, 9);
+randomImage.style.backgroundImage = `url('images/${randomNumber}.png')`;
+
+const imageNumberElement = document.querySelectorAll('.image-num');
+
+imageNumberElement.forEach((item) => {
+    const num = item.innerHTML;
+    if (num === randomNumber.toString()) {
+        item.style.backgroundColor = 'aqua';
+    }
+})
