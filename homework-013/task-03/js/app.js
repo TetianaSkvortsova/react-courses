e0d580e577d5539cf04e094837c4b20b38c1b59e
Tetiana <jany.starling@gmail.com>
2025-09-07 16:48:22 +0300

    Homework Lecture 13 Todo List redone

--- homework-013/task-03/js/app.js
+++ homework-013/task-03/js/app.js
@@ -13,32 +13,53 @@
 'use strict'
 
 const btnAddTask = document.querySelector('.addTask');
-const todoList = document.querySelector('.todoList');
+const taskItems = document.querySelector('ul');
+
+showTodoList(todoList);
 
 btnAddTask.addEventListener('click', () => {
     const newTask = document.querySelector('input');
     const taskValue = newTask.value.trim();
-    if(!taskValue) {
+    if (!taskValue) {
         return;
     }
     addTask(taskValue);
     newTask.value = '';
 });
 
-function addTask(value) {
-    const newTaskItem = document.createElement('li');
-    const deleteBtn = document.createElement('button');
-    deleteBtn.setAttribute('type', 'button');
-    deleteBtn.setAttribute('class', 'deleteBtn font');
-    deleteBtn.innerHTML = 'Delete';
-    newTaskItem.innerHTML = value;
-    todoList.appendChild(newTaskItem);
-    newTaskItem.appendChild(deleteBtn);
-}
-
-todoList.addEventListener('click', (event) => {
+taskItems.addEventListener('click', (event) => {
     if (event.target && event.target.tagName === 'BUTTON') {
         const parentElement = event.target.parentElement;
+        const taskId = parentElement.getAttribute('id');
+        const index = todoList.findIndex(index => index = taskId);
+
+        todoList.splice(index, 1);
         parentElement.remove();
     }
 })
+
+function showTodoList(list) {
+    taskItems.textContent = '';
+    list.forEach((item) => {
+        const taskItem = document.createElement('li');
+        const taskValue = document.createElement('span');
+        const deleteBtn = document.createElement('button');
+
+        deleteBtn.setAttribute('type', 'button');
+        deleteBtn.setAttribute('class', 'deleteBtn font');
+        deleteBtn.innerHTML = 'Delete';
+
+        taskValue.innerHTML = item.task;
+        taskItem.setAttribute('id', item.id);
+        taskItem.appendChild(taskValue);
+        taskItem.appendChild(deleteBtn);
+
+        taskItems.appendChild(taskItem);
+    })
+}
+
+function addTask(value) {
+    const uniqueId = crypto.randomUUID();
+    todoList.push({id: uniqueId, task: value});
+    showTodoList(todoList);
+}
---
+++ homework-013/task-03/js/todoList.js
@@ -0,0 +1,14 @@
+const todoList = [
+    {
+        id: '1',
+        task: 'item 1',
+    },
+    {
+        id: '2',
+        task: 'item 2',
+    },
+    {
+        id: '3',
+        task: 'item 3',
+    }
+]
