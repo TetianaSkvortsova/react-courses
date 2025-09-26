4167920b61a03ef0dcd8d9ed094ed292ddcf83d9
Tetiana <jany.starling@gmail.com>
2025-09-26 12:23:47 +0300

    Homework Lecture 19 timer done

--- homework-019-asynchrony/task-01/app.js
+++ homework-019-asynchrony/task-01/app.js
@@ -6,17 +6,33 @@
 // Відобразити на сторінці час у форматі 01:25
 // Коли закінчився таймер зупинити його
 
-let start = 10;
+const setTimerBtn = document.querySelector('.set-timer button');
 
-function timer() {
+setTimerBtn.addEventListener('click', () => {
+    const setTimerInput = document.querySelector('.set-timer input');
+    const start = Number(setTimerInput.value);
+
+    startTimer(start, setTimerInput);
+})
+
+function startTimer(start, setTimerInput) {
+    const setTimerContainer = document.querySelector('.set-timer');
     const timerContainer = document.querySelector('.timer');
-    const seconds = ('0' + start % 60).slice(-2);
-    const minutes = (start - seconds) >= 60 ? ('0' + (start - seconds) / 60).slice(-2) : `00`;
-    timerContainer.textContent = `${minutes} : ${seconds}`;
-    if (start === 0) {
-        clearInterval(interval);
+    setTimerContainer.style.display = 'none';
+    timerContainer.style.display = 'block';
+    const intervalHandler = () => {
+        const seconds = ('0' + start % 60).slice(-2);
+        const minutes = (start - seconds) >= 60 ? ('0' + (start - seconds) / 60).slice(-2) : `00`;
+        timerContainer.textContent = `${minutes} : ${seconds}`;
+        if (start === 0) {
+            clearInterval(interval);
+            setTimerContainer.style.display = 'block';
+            setTimerInput.value = '';
+            timerContainer.style.display = 'none';
+        }
+        start -= 1;
+        console.log(start);
     }
-    start -= 1;
+    intervalHandler();
+    const interval = setInterval(intervalHandler, 1000);
 }
-
-const interval = setInterval(timer, 1000);
